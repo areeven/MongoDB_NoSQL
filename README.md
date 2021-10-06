0f97af003bbb5348552699078a4c3cc12c6b0cee
Areeven <emilie.ost1990@gmail.com>
2021-10-06 13:15:25 +0200

    added to readme

--- README.md
+++ README.md
@@ -13,4 +13,17 @@ Step 1
    2. .gitignore - Add ignore for
       1. .idea
       2. DS_Store
-6. 
+6. Push to gitHub
+
+Step 2
+1. In console window start by showing which databases exist in our storage
+   1. show dbs = show databases ctrl+enter (or cmd+enter on Mac) to run - everytime a new method is added
+2. Create database by entering command
+   1. use <name-of-database> 
+   2. Introspect database when prompted
+3. Now we can start adding what we want our database to contain
+4. Start by creating collection, there are three different ways to do so
+   1. db.createCollection('my_collection')
+   2. db.createCollection('my_collection', {capped: true, size: 3, max: 3})
+   3. db.my_collection.insertOne({'Name': 'my_collection Created'})
+5. 
--- mongoTest.sql
+++ mongoTest.sql
@@ -18,7 +18,82 @@ show collections
 db.dropDatabase()
 show collections
 
+use new_db
+db.gaming.insertOne({'Player': 'Olaf', 'Class': 'Warrior', 'Level': 20})
+show collections
 
+db.gaming.find()
 
+// Insert into collections
+use test
+show collections
+
+db.my_collection.find()
+db.my_collection.insertOne({Username: 'Adam', Password: 'password'})
+db.my_collection.find()
+
+db.my_collection.insertMany([
+    {Username: 'Bengt', Password: 'password'},
+    {Username: 'Cecilia', Password: 'password'},
+    {Username: 'David', Password: 'password'}
+])
+
+db.my_collection.find()
+
+// Query document
+db.my_collection.find()
+// Add .pretty() when working in console to make it look cleaner - db.my_collection.find().pretty()
+
+db.my_collection.find({Username: 'Bengt'})
+db.my_collection.find({Password: 'password'})
+
+// Update
+db.my_collection.updateOne({Username: 'Bengt'}, {$set: {Password: 'hemligt'}})
+db.my_collection.updateOne({Username: 'Cecilia'}, {$set: {Password: 'secret'}})
+db.my_collection.updateOne({Username: 'David'}, {$set: {Password: 'h3ml1gt'}})
+
+// Filter
+// Equality
+db.my_collection.find({Password: 'hemligt'})
+// Less than
+db.my_collection.find({Password: {$lt: '4'}})  // lt = less than (Get out all elements that are below 4
+db.my_collection.find({Password: {$lte: '4'}})  // lte = less than equals(Get out all elements that are below 4 or 4
+db.my_collection.find({Password: {$gt: '4'}})  // gt = greater than equal(Get out all elements that are above 4
+db.my_collection.find({Password: {$gte: '4'}})  // gt = greater than equal(Get out all elements that are above 4 or 4
+db.my_collection.find({Password: {$ne: '4'}})  // ne = not equal(Get out all elements that are not equal 4 or 4
+
+// Find all elements AND
+db.my_collection.find({$and: [{Username: {$ne: 'Adam'}}, {Password: {$ne: 'secret'}}]})
+
+// Find all elements OR
+db.my_collection.find({$or: [{Username: 'Adam'}, {Password: 'secret'}]})
+
+// Update
+db.my_collection.find().count()
+db.my_collection.insertOne({Username: 'Emilie', Password: 'hoppa123'})
+db.my_collection.find().count()
+
+// Regular update
+db.my_collection.updateOne({Username: 'Cecilia'}, {$set: {Password: 'secret'}})
+
+// Javascript update function and variables
+selection_criteria = {Username: 'Cecilia'}
+updated_data = {Username: 'Carl'}
+
+db.my_collection.find(selection_criteria)
+db.my_collection.updateOne(selection_criteria, {$set: updated_data})
+db.my_collection.find(updated_data)
+
+db.my_collection.find()
+
+// Delete
+db.my_collection.find().count()
+db.my_collection.insertOne({Username: 'Family Guy', Password: 'dontguess'})
+
+selection_criteria = {Username: 'Family Guy'}
+db.my_collection.find(selection_criteria)
+db.my_collection.deleteOne(selection_criteria)
+db.my_collection.find().count()
+db.my_collection.find()
 
 
